45ece7011b5589774737ee0e6a2d4e8a2fc4e9ea
Mukul <109015634+Mukul0909@users.noreply.github.com>
2022-11-12 21:01:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
-let goodthought = ['Bravo!', 'Awesome!', 'Nailed It!', 'Marvellous', 'Extraordinary!', 'Killing Spree!'];
-        let downthought = ['Boo-Ya!', 'Come On!', 'Better Luck Next Time!', 'Damn You!', 'Think Hard!', 'You Gotta Be Kidding!'];
+        let goodthought = [
+          "Bravo!",
+          "Awesome!",
+          "Nailed It!",
+          "Marvellous",
+          "Extraordinary!",
+          "Killing Spree!",
+        ];
+        let downthought = [
+          "Boo-Ya!",
+          "Come On!",
+          "Better Luck Next Time!",
+          "Damn You!",
+          "Think Hard!",
+          "You Gotta Be Kidding!",
+        ];
 
         let thought = document.getElementById("thought");
         let btn = document.getElementById("next");
@@ -8,93 +22,101 @@ let goodthought = ['Bravo!', 'Awesome!', 'Nailed It!', 'Marvellous', 'Extraordin
         let boxTwo = document.getElementById("2");
         let boxThree = document.getElementById("3");
         let boxFour = document.getElementById("4");
-        let count = document.getElementById('score');
+        let count = document.getElementById("score");
         let score = 0;
         let detector = 0;
         let colour = {};
-       
-        btn.addEventListener("click", () => {
-            detector = 0;
-            for(let i=0;i<4;i++){
-                let arr = [];
-                    for (let j=0; j<3; ++j) {
-                    arr.push(Math.floor(Math.random()*255));
-                    }
-                colour[i] = arr.join(",")
-            }
+        let matter = document.getElementById("matter");
+        let intro = document.querySelector(".intro");
+        let start = document.querySelector(".start");
+        let bulb = document.querySelector(".material-symbols-outlined");
 
-        display.innerHTML = colour[Math.floor(Math.random()*3)];
-
-        boxOne.setAttribute('style',`background-color: rgb(${colour[0]})`);
-        boxTwo.setAttribute('style',`background-color: rgb(${colour[1]})`);
-        boxThree.setAttribute('style',`background-color: rgb(${colour[2]})`);
-        boxFour.setAttribute('style',`background-color: rgb(${colour[3]})`);        
+        bulb.addEventListener("click", () => {
+          intro.classList.remove("hide");
+          matter.classList.add("main-content");
         });
 
-        boxOne.addEventListener('click', ()=>{
-            detector = boxOne.id;
-            if(colour[detector-1]==display.innerHTML){
-                score+=1;
-                count.innerHTML = score;
-                let say = goodthought[Math.floor(Math.random()*6)];
-                thought.innerHTML = say;
+        start.addEventListener("click", () => {
+          intro.classList.add("hide");
+          matter.classList.remove("main-content");
+          generatecolor();
+        });
 
+        function generatecolor() {
+          detector = 0;
+          for (let i = 0; i < 4; i++) {
+            let arr = [];
+            for (let j = 0; j < 3; ++j) {
+              arr.push(Math.floor(Math.random() * 255));
             }
+            colour[i] = arr.join(",");
+          }
 
-            else{
-                let say = downthought[Math.floor(Math.random()*6)];
-                thought.innerHTML = say;
-            }
-            // console.log(detector);
-        })
-       
-        boxTwo.addEventListener('click', ()=>{
-            detector = boxTwo.id
-            if(colour[detector-1]==display.innerHTML){
-                score+=1;
-                count.innerHTML = score;
-                let say = goodthought[Math.floor(Math.random()*6)];
-                thought.innerHTML = say;
-            }
+          display.innerHTML = colour[Math.floor(Math.random() * 3)];
 
-            else{
-                let say = downthought[Math.floor(Math.random()*6)];
-                thought.innerHTML = say;
-            }
-            // console.log(detector);
-        })
+          boxOne.setAttribute("style", `background-color: rgb(${colour[0]})`);
+          boxTwo.setAttribute("style", `background-color: rgb(${colour[1]})`);
+          boxThree.setAttribute("style", `background-color: rgb(${colour[2]})`);
+          boxFour.setAttribute("style", `background-color: rgb(${colour[3]})`);
+        }
 
-        boxThree.addEventListener('click', ()=>{
-            detector = boxThree.id
-            if(colour[detector-1]==display.innerHTML){
-                score+=1;
-                count.innerHTML = score;
-                let say = goodthought[Math.floor(Math.random()*6)];
-                thought.innerHTML = say;
-            }
+        btn.addEventListener("click", generatecolor);
 
-            else{
-                let say = downthought[Math.floor(Math.random()*6)];
-                thought.innerHTML = say;
-            }
-            // console.log(detector);
-        })
+        boxOne.addEventListener("click", () => {
+          detector = boxOne.id;
+          if (colour[detector - 1] == display.innerHTML) {
+            score += 1;
+            count.innerHTML = score;
+            let say = goodthought[Math.floor(Math.random() * 6)];
+            thought.innerHTML = say;
+          } else {
+            let say = downthought[Math.floor(Math.random() * 6)];
+            thought.innerHTML = say;
+          }
+          // console.log(detector);
+        });
 
-        boxFour.addEventListener('click', ()=>{
-            detector = boxFour.id
-            if(colour[detector-1]==display.innerHTML){
-                score+=1;
-                count.innerHTML = score;
-                let say = goodthought[Math.floor(Math.random()*6)];
-                thought.innerHTML = say;
-            }
+        boxTwo.addEventListener("click", () => {
+          detector = boxTwo.id;
+          if (colour[detector - 1] == display.innerHTML) {
+            score += 1;
+            count.innerHTML = score;
+            let say = goodthought[Math.floor(Math.random() * 6)];
+            thought.innerHTML = say;
+          } else {
+            let say = downthought[Math.floor(Math.random() * 6)];
+            thought.innerHTML = say;
+          }
+          // console.log(detector);
+        });
 
-            else{
-                let say = downthought[Math.floor(Math.random()*6)];
-                thought.innerHTML = say;
-            }
-            // console.log(detector);
-        })
+        boxThree.addEventListener("click", () => {
+          detector = boxThree.id;
+          if (colour[detector - 1] == display.innerHTML) {
+            score += 1;
+            count.innerHTML = score;
+            let say = goodthought[Math.floor(Math.random() * 6)];
+            thought.innerHTML = say;
+          } else {
+            let say = downthought[Math.floor(Math.random() * 6)];
+            thought.innerHTML = say;
+          }
+          // console.log(detector);
+        });
+
+        boxFour.addEventListener("click", () => {
+          detector = boxFour.id;
+          if (colour[detector - 1] == display.innerHTML) {
+            score += 1;
+            count.innerHTML = score;
+            let say = goodthought[Math.floor(Math.random() * 6)];
+            thought.innerHTML = say;
+          } else {
+            let say = downthought[Math.floor(Math.random() * 6)];
+            thought.innerHTML = say;
+          }
+          // console.log(detector);
+        });
 
         // console.log(detector)
         // if(colour[detector-1]==display.innerHTML){
